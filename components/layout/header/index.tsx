f648a6a3f9a210466de9e969f2e981a541add4a9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2025-12-20 15:50:08 -0300

    :sparkles: feat: develops mobile sidebar with transition

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -5,53 +5,48 @@ import { List } from 'phosphor-react';
 import { Logo } from '@/components/textual/logo';
 import { IconButton } from '@/components/buttons/icon-button';
 import { OutlineSelect } from '../../forms/outline-select/index';
-import { navbarSections } from '@/utils/consts/sections';
+import { navbarSections } from '@/consts/sections';
 import { NavbarTab } from '@/components/textual/navbar-tab';
-import { SectionsKey } from '@/utils/@types/sections';
-import { useEffect, useRef } from 'react';
+import { useScroll } from '@/hooks/useScrollNavigation';
+import { MobileSidebar } from '../mobile-sidebar';
+
+import { useTransition } from '@/hooks/useTransition';
 
 export function Header() {
-  const currentSection: SectionsKey = 'home';
-  const headerRef = useRef<HTMLElement>(null);
+  const { activeSection, isSticky } = useScroll();
 
-  useEffect(() => {
-    const checkScroll = () => {
-      if (!headerRef.current) return;
+  const [shouldRenderSidebar, toggleSidebarVisibility, isTransitioning] = useTransition(false, 300);
 
-      if (window.scrollY > 50) {
-        headerRef.current.classList.add(styles.scrolled);
-      } else {
-        headerRef.current.classList.remove(styles.scrolled);
-      }
-    };
-
-    checkScroll();
-
-    window.addEventListener('scroll', checkScroll);
-
-    return () => window.removeEventListener('scroll', checkScroll);
-  }, []);
+  const onClose = () => toggleSidebarVisibility(false);
 
   return (
-    <header ref={headerRef} className={`${styles.header}`}>
-      <Logo />
-      <IconButton onClick={() => { }}>
-        <List size={24} weight="bold" />
-      </IconButton>
-      <nav className={`navbar ${styles.navbar}`}>
-        <ul>
-          {navbarSections.map((item) => (
-            <li key={item.value}>
-              <NavbarTab
-                isCurrent={currentSection === item.value}
-                label={item.label}
-                value={item.value}
-              />
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <OutlineSelect />
-    </header>
+    <>
+      <header className={`${styles.header} ${isSticky ? 'scrolled' : ''}`}>
+        <Logo />
+        <IconButton onClick={() => toggleSidebarVisibility(true)}>
+          <List size={24} weight="bold" />
+        </IconButton>
+        <nav className={`navbar ${styles.navbar}`}>
+          <ul>
+            {navbarSections.map((item) => (
+              <li key={item.value}>
+                <NavbarTab
+                  isCurrent={activeSection === item.value}
+                  label={item.label}
+                  value={item.value}
+                />
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <OutlineSelect />
+      </header>
+      {shouldRenderSidebar && (
+        <MobileSidebar
+          isOpen={isTransitioning}
+          onClose={onClose}
+        />
+      )}
+    </>
   );
 }
---
+++ components/layout/mobile-sidebar/index.tsx
@@ -0,0 +1,64 @@
+import { X } from 'phosphor-react';
+import { SocialLinks } from '../social-links';
+import styles from './mobile-sidebar.module.css';
+import { navbarSections } from '@/consts/sections';
+import { NavbarTab } from '@/components/textual/navbar-tab';
+import { useScroll } from '@/hooks/useScrollNavigation';
+import { IconButton } from '@/components/buttons/icon-button';
+import { Logo } from '@/components/textual/logo';
+
+interface MobileSidebarProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function MobileSidebar({ isOpen, onClose }: MobileSidebarProps) {
+  const { activeSection } = useScroll();
+  
+  return (
+    <>
+      <div onClick={onClose} className={`${styles.backdropBlur} ${isOpen ? styles.show : ''} `} aria-hidden></div>
+      <aside
+        // ref={sidebarRef}
+        id="main-sidebar"
+        className={`${styles.sidebar} ${isOpen ? styles.show : ''}`}
+        aria-hidden={!isOpen}
+        aria-modal="true"
+        role="dialog"
+        aria-labelledby="sidebar-title"
+      >
+        <header className={styles.sidebarHeader}>
+          <h2 id="sidebar-title" className="sr-only">Menu de Navegação</h2>
+          <Logo />
+          <IconButton
+            onClick={onClose}
+            aria-label="Fechar menu"
+          >
+            <X size={24} aria-hidden="true" weight="bold" />
+          </IconButton>
+        </header>
+
+        <nav className={styles.sidebarNav}>
+          <ul role="list">
+            {navbarSections.map((section) => (
+              <li key={section.value}>
+                <NavbarTab
+                  isMobile
+                  isCurrent={activeSection === section.value}
+                  label={section.label}
+                  value={section.value}
+                  onClick={onClose}
+                />
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <footer className={styles.sidebarFooter}>
+          <p className="sr-only">Our social media channels:</p>
+          <SocialLinks />
+        </footer>
+      </aside>
+    </>
+  );
+}
