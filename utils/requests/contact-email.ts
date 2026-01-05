656fbfa465f262bf3001b19c016890a18a59ae3e
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-01-05 14:12:24 -0300

    :sparkles: feat: develops email template

--- utils/requests/contact-email.ts
+++ utils/requests/contact-email.ts
@@ -1,9 +1,9 @@
-import { ContactFormData } from '@/types/contact';
+import { SendEmailProps } from '@/app/api/send-email/route';
 import axios from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export async function sendContactEmail(data: ContactFormData) {
+export async function sendContactEmail(data: SendEmailProps) {
   const response = await axios.post(`/api/send-email`, data, {
     baseURL
   });
