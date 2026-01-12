7088a4872045dc48194b2a02facefbabedf136ee
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-01-12 15:06:19 -0300

    :sparkles: feat: anchor variant to button component

--- components/buttons/button/index.tsx
+++ components/buttons/button/index.tsx
@@ -1,14 +1,30 @@
+import Link from 'next/link';
 import styles from './button.module.css';
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
-type HTMLButtonProps = ComponentPropsWithoutRef<'button'>;
+type HTMLButtonProps = {
+  component?: 'button';
+} & ComponentPropsWithoutRef<'button'>;
 
-type ButtonProps = HTMLButtonProps & {
+type HTMLAnchorProps = {
+  component: 'a';
+  href: string;
+} & ComponentPropsWithoutRef<'a'>;
+
+type ButtonProps = (HTMLButtonProps | HTMLAnchorProps) & {
   children: ReactNode;
   theme?: 'outline' | 'gradient' | 'transparent';
 }
 
 export function Button({ children, theme = 'gradient', ...props }: ButtonProps) {
+  if (props.component === 'a') {
+    return (
+      <Link {...props} className={`button ${theme} ${styles.button}`}>
+        {children}
+      </Link>
+    )
+  }
+
   return (
     <button {...props} className={`button ${theme} ${styles.button}`}>
       {children}
