92afb7a08304f8824e9fb16931d03f6400bcdde9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2026-01-21 12:11:48 -0300

    :sparkles: feat: add link to logo

--- components/textual/logo/index.tsx
+++ components/textual/logo/index.tsx
@@ -2,20 +2,26 @@ import Image from "next/image";
 import styles from "./logo.module.css";
 
 import LogoImage from "../../../public/Logo.svg";
+import Link from "next/link";
+import { useTranslate } from "@/hooks/useTranslate";
 
 export function Logo() {
+  const { locale } = useTranslate();
+
   return (
-    <figure className={styles.logo}>
-      <Image
-        src={LogoImage}
-        alt="Rodrigo Moreira's Logo"
-        height={36}
-        width={36}
-        className={styles.logoImage}
-      />
-      <figcaption className={styles.logoTypo}>
-        <span>.</span>Rodrigo<span>()</span>
-      </figcaption>
-    </figure>
+    <Link href={`/${locale}`} className={styles.logoContainer}>
+      <figure className={styles.logo}>
+        <Image
+          src={LogoImage}
+          alt="Rodrigo Moreira's Logo"
+          height={36}
+          width={36}
+          className={styles.logoImage}
+        />
+        <figcaption className={styles.logoTypo}>
+          <span>.</span>Rodrigo<span>()</span>
+        </figcaption>
+      </figure>
+    </Link>
   );
 }
